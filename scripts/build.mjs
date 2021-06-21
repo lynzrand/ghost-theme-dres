6d0596f87d9dcc6989be210036f3976aaf0a379b
Rynco Maekawa <lynzrand@outlook.com>
2021-06-21 21:58:39 +0800

    format index page

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,32 +1,25 @@
 import less from 'less'
 import { readFileSync, writeFileSync } from 'fs'
-import ts from 'typescript'
-{
-    console.log("Compiling LESS -> CSS")
-    let result = await less.render(readFileSync("assets/main.less", { encoding: 'utf-8' }))
-    writeFileSync("assets/main.css", result.css)
-}
+import { exec } from 'child_process'
+import { stderr, stdout } from 'process'
+// import Parcel from '@parcel/core'
+// {
+//     console.log("Compiling LESS -> CSS")
+//     let result = await less.render(
+//         readFileSync("assets/main.less", { encoding: 'utf-8' }),
+//         { "sourceMap": { "outputSourceFiles": true } }
+//     )
+//     writeFileSync("assets/main.css", result.css)
+// }
 
-{
-    console.log("Compiling TypeScript -> JS")
-    let tsProgram = ts.createProgram(['./assets/scripts/main.ts'], {})
-    let emitResult = tsProgram.emit()
+// {
+//     console.log("Compiling TypeScript -> JS")
+//     // let bundler = new Parcel({
+//     //     entries: "assets/scripts/main.ts"
+//     // })
+//     // await bundler.run()
+//     let process = exec("yarn parcel build  assets/scripts/main.ts --dist-dir assets/scripts")
+//     process.stdout.pipe(stdout)
+//     process.stderr.pipe(stderr)
 
-    let allDiagnostics = ts
-        .getPreEmitDiagnostics(tsProgram)
-        .concat(emitResult.diagnostics);
-
-    allDiagnostics.forEach(diagnostic => {
-        if (diagnostic.file) {
-            let { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start);
-            let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
-            console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-        } else {
-            console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
-        }
-    });
-
-    let exitCode = emitResult.emitSkipped ? 1 : 0;
-    console.log(`Process exiting with code '${exitCode}'.`);
-    process.exit(exitCode);
-}
+// }
--- scripts/zip.mjs
+++ scripts/zip.mjs
@@ -6,10 +6,16 @@ let out = fs.createWriteStream("dist.zip")
 let archive = archiver('zip', { zlib: { level: 9 } })
 
 archive.pipe(out)
-archive.on("entry", (entry) => { console.log(entry.name) })
+archive.on("entry", (entry) => {
+    console.log("Added: ", entry.name)
+})
+archive.on("error", (data) => {
+    console.log(`Error: ${data.message}`)
+})
 
 // include assets, but not main.less
 archive.directory('assets/fonts')
+archive.glob('assets/scripts/**.js')
 archive.file('assets/google-fonts.css')
 archive.file('assets/main.css')
 
